85eb7b9f5a4130e17cca5af2f82e8388ed3bf433
Shahadat <shahadat2cp17@gmail.com>
2023-12-21 12:26:22 +0600

    gettig local commands boiler

---
+++ src/utils/getLocalCommands.js
@@ -0,0 +1,42 @@
+/* -------------------------------------------------------------------------- */
+/*             getting all command files from local -- biolarplate            */
+/* -------------------------------------------------------------------------- */
+const path = require("path");
+const getAllFiles = require("./getAllFiles");
+module.exports = function (exceptions = []) {
+    const localCommands = []
+    try {
+
+        const commandsFolders = getAllFiles(path.join(__dirname, "..", "commands"));
+        for (const commandfolder of commandsFolders) {
+            try {
+                const commandFiles = getAllFiles(commandfolder);
+                for (const commandFile of commandFiles) {
+                    try {
+
+                        const filePath = path.join(commandfolder, commandFile.name)
+
+                        const commandObject = require(filePath)
+
+                        if (exceptions.includes(commandObject.name)) {
+                            continue;
+                        }
+
+                        localCommands.push(commandObject)
+
+                    } catch (error) {
+                        console.log(error);
+                        return localCommands = [];
+                    }
+                }
+            } catch (error) {
+                console.log(error);
+                return localCommands = [];
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        return localCommands = [];
+    }
+    return localCommands
+}
